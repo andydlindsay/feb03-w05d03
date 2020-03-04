9e2e851903cbfe64916c9b7ed506e667292489aa
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-04 15:05:29 -0800

    basic functionality finished

--- routes/villains.js
+++ routes/villains.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { browse, read } = require('../db/queries');
+const { browse, read, edit, add, del } = require('../db/queries');
 
 router.get('/', (req, res) => {
   browse((err, villains) => {
-    // res.json(villains);
     if (err) {
       return res.render('error', { err });
     }
@@ -12,10 +11,44 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/new', (req, res) => {
+  res.render('new-villain');
+});
+
 router.get('/:id', (req, res) => {
-  read(req.params.id, (villain) => {
-    console.log(villain);
+  read(req.params.id, (err, villain) => {
+    if (err) {
+      return res.render('error', { err });
+    }
     res.render('villain', villain);
+  });
+});
+
+router.post('/:id', (req, res) => {
+  edit(req.params.id, req.body.villain, (err) => {
+    if (err) {
+      return res.render('error', { err });
+    }
+    res.redirect(`/villains/${req.params.id}`);
+  });
+});
+
+router.post('/', (req, res) => {
+  const { villain, movie } = req.body;
+  add(villain, movie, (err) => {
+    if (err) {
+      return res.render('error', { err });
+    }
+    res.redirect('/villains');
+  });
+});
+
+router.post('/:id/delete', (req, res) => {
+  del(req.params.id, (err) => {
+    if (err) {
+      return res.render('error', { err });
+    }
+    res.redirect('/villains');
   });
 });
 
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const villainRouter = require('./routes/villains');
 
+const app = express();
 const port = process.env.PORT || 6543;
 
-app.use('/villains', villainRouter);
+// middleware
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// view engine
 app.set('view engine', 'ejs');
+
+// routes
+app.use('/villains', villainRouter);
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
